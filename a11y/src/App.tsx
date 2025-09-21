25935a10720395e3b1a12d68d1d370c7a1e65565
yeongipark <yeongi0111@naver.com>
2025-09-21 19:14:46 +0900

    chore: 시멘틱 태그 적용

--- a11y/src/App.tsx
+++ a11y/src/App.tsx
@@ -6,11 +6,11 @@ import FlightBooking from "./components/FlightBooking";
 function App() {
   return (
     <div className="app">
-      <div className="app-main">
-        <div className="flight-booking-container">
+      <main className="app-main">
+        <section className="flight-booking-container">
           <FlightBooking />
-        </div>
-      </div>
+        </section>
+      </main>
     </div>
   );
 }
